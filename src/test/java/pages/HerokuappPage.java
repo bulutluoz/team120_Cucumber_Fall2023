fba2b3fc85c6f05d0fdf23155aba2eea124107a8
bulutluoz <abbulutluoz@gmail.com>
2023-10-17 20:18:44 +0200

    22.gun sonu

---
+++ src/test/java/pages/HerokuappPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HerokuappPage {
+    public HerokuappPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+     @FindBy(xpath = "//*[@onclick='addElement()']")
+     public WebElement addElementButonu;
+
+    @FindBy(xpath = "//*[text()='Delete']")
+    public WebElement deleteButonu;
+}
--- src/test/resources/features/US1007_ScenarioOutlineIleNegatifLoginTesti.feature
+++ src/test/resources/features/US1007_ScenarioOutlineIleNegatifLoginTesti.feature
@@ -1,6 +1,6 @@
 Feature: US1007 liste olarak verilen kullanici adi ve sifrelerle giris yapilamadigini test eder
 
-  @wip
+
   Scenario Outline: TC11 qualitydemy sayfasina yanlis bilgilerle giris yapilamaz
 
     Given kullanici "qdUrl" anasayfaya gider
